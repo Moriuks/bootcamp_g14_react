f6072d79a748fdecb85d26863abcdf8f9a35b274
Luis Mora <luismora371@gmail.com>
2021-12-13 19:40:47 -0600

    update

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+
+// Styles
+import "./Navbar.css";
+export default function Counter() {
+	const [counter, setCounter] = useState(0);
+	
+
+
+	return (
+		<div>
+			<ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+		</div>
+	);
+}
