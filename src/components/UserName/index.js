74888fd3fb8c49c754a7fc30027d8432711f6dc1
Luis Mora <luismora371@gmail.com>
2021-12-14 19:22:02 -0600

    added styles

--- src/components/UserName/index.js
+++ src/components/UserName/index.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "./userStyles.css";
 
 function UserName({ name, status, species, image }) {
 	return (
-		<div>
-		 	<img src={image} alt="Gir" width="200" height="300"/> 
-			<h1>
-				{name}
-			</h1>
-			<p>
-			    {status} {species}
-			</p>
+		<div class="row">
+  			<div class="column">
+			  <div class="card-img">
+		 		<img src={image} alt="Gir" width="200" height="300"/> 
+				<div class="card-content">
+					<h4><b>{name}</b></h4>
+					<p>{status} & {species}</p>
+				</div>
+			</div>
+			</div>
 		</div>
+		
 		
 	);
 }
