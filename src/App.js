4e963b8887f5d2c013fab8615925da173ad738fa
Luis Mora <luismora371@gmail.com>
2021-12-15 19:34:32 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
-
-// Components
-import NavBar from "./components/NavBar";
-import UserName from "./components/UserName";
+import { useState } from "react";
 
 function App() {
-	const [users, setUsers] = useState([]);
-	const [data] = useState({});
+	const [input1, setInput1] = useState("fdsafasfs");
+	const [input2, setInput2] = useState(0);
 
-	useEffect(() => {
-		// const getUsers = async () => {
-		// 	const response = await fetch("https://rickandmortyapi.com/api/character");
-		// 	console.table(response.results);
-		// 	const data = await response.json();
-		// 	console.log(data.results);
-		// 	setUsers(data.results);
-		// };
-
-		getUsers();
-	}, []);
-
-	const usersUI = users.map(({ id, name, status, species, image }) => (
-		<UserName key={id} name={name} status={status} species={species} image={image}   />
-	));
-
-	const getUsers = async (page) => {
-		console.log('Entro por otra dosis');
-		const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
-		console.table(response.results);
-		const data = await response.json();
-		console.log(data.results);
-		setUsers(data.results);
-	};
-
-	const handleClick = async (action) => {
-		console.log('botonazo');
-		if (action === "next" && data.next != null) {
-			const page = data.next.split("?")[1];
-			const res = await getUsers(page);
-			console.log(res);
-			// setCharacters(results);
-			// setData(info);
-		}
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(event);
 	};
 
 	return (
-		<div className="App">
-			<div className="fixed-container">
-				<button onClick={() => handleClick("Prev")} className="btn">
-					Prev
-				</button>
-				<button onClick={() => handleClick("next")} className="btn">
-					Next
-				</button>
-			</div>
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<NavBar />
-				{usersUI}
-			</header>
+		<div className="container">
+			<form className="form" onSubmit={handleSubmit}>
+				<div className="flex">
+					<label htmlFor="input1">Input:</label>
+					<input
+						id="input1"
+						onChange={(e) => setInput1(e.target.value)}
+						value={input1}
+					/>
+					<input
+						type={"number"}
+						id="input2"
+						onChange={(e) => setInput2(e.target.value)}
+						value={input2}
+					/>
+					<button type="submit" className="btn">
+						Enviar
+					</button>
+				</div>
+			</form>
+			Cantidad: {input2}
 		</div>
 	);
 }
--- src/components/UserName/index.js
+++ src/components/UserName/index.js
@@ -1,21 +1,12 @@
 import React from "react";
-import "./userStyles.css";
 
-function UserName({ name, status, species, image }) {
+function UserName({ firstName, lastName }) {
 	return (
-		<div className="row">
-  			<div className="column">
-			  <div className="card-img">
-		 		<img src={image} alt="Gir" width="200" height="300"/> 
-				<div className="card-content">
-					<h4><b>{name}</b></h4>
-					<p>{status} & {species}</p>
-				</div>
-			</div>
-			</div>
+		<div>
+			<h1>
+				{firstName} {lastName}
+			</h1>
 		</div>
-		
-		
 	);
 }
 
