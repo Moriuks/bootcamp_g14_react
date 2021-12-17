c1299d1dd3faa2601d6e028895ca58f7664f985a
Luis Mora <luismora371@gmail.com>
2021-12-16 19:25:02 -0600

    Weird forms update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,34 @@
 import React from "react";
 
-function Input(){
-    return(
-        <div>
-            <label> </label>
-        </div>
-    )
+// CSS
+import "./Input.css";
+
+function Input({ id, label, value, setValue, type = "text" }) {
+
+	return (
+		<div className="input-container">
+			<label className="label" htmlFor={id}>
+				{label}:
+			</label>
+			<input
+				id={id}
+				className="input"
+				onChange={(event) => setValue(event.target.value)}
+				value={value}
+			/>
+            <input
+				id={id}
+                type = {type}
+				className="input"
+				onChange={(event) => setValue(event.target.value)}
+				value={value}
+			/>
+            <select  onChange={(event) => setValue(event.target.value)} value={value}>
+                    <option value="grapefruit">Male</option>
+                    <option value="lime">Gender</option>
+            </select>
+		</div>
+	);
 }
 
 export default Input;
--- src/pages/UsersNew/index.js
+++ src/pages/UsersNew/index.js
@@ -1,17 +1,60 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import Input from "../../components/Input";
  
+// CSS
+import "./UsersNew.css";
 
 export default function UsersNew() {
-    const [firstName, setFirstName] = useState("david");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+    const [occupation, setoccupation] = useState("");
+	const [gender, setGender] = useState("Male");
+    const [birthdate, setBirthdate] = useState("");
 
-	return (
-    <div className="container flex-col">
-        <h1> Crear un usuario</h1>
-        <form>
-            <Input
+    
+    
+    return (
+		<div className="container flex-col">
+			<h1>Crea un usuario</h1>
+			<form>
+				<Input
+					id="firstName"
+					label="First Name"
+					value={firstName}
+					setValue={setFirstName}
+				/>
+				<Input
+					id="lastName"
+					label="Last Name"
+					value={lastName}
+					setValue={setLastName}
+				/>
+                <Input
+					id="occupation"
+					label="Occupation"
+					value={occupation}
+					setValue={setoccupation}
+				/>
+                <Input
+					id="birthdate"
+                    type="date"
+					label="Birthdate"
+					value={birthdate}
+					setValue={setBirthdate}
+				/>
+                <Input
+					id="gender"
+					label="Gender"
+					value={gender}
+					setValue={setGender}
+				/>
                 
-            />
-        </form>
-    </div>);
+                
+                {/* gender
+                occupation
+                birthdate */}
+			</form>
+		</div>
+	);
 }
