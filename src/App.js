691aa23839173ce077b3482cc3cda84c5317d7f6
Moriuks <51276630+Moriuks@users.noreply.github.com>
2022-01-20 22:11:09 -0600

    Updated app.js NUT-51
    NUT-51

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/calculator';
 // this is just a test
+// NUT-51
 function App() {
   return (
     <div className="App">
